0f0595e150f7370850e058215d00b9cd70db2068
masonsherbondy <sherbondy.mason@gmail.com>
2021-09-27 14:50:32 -0500

    Correct form

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -19,11 +19,14 @@ USE employees;
 #1. Find all the current employees with the same hire date as employee 101010 using a sub-query.
 SELECT first_name, last_name, hire_date
 FROM employees
-JOIN dept_emp USING(emp_no)
-WHERE to_date > NOW() AND hire_date = (
+WHERE hire_date = (
 		SELECT hire_date
 		FROM employees
 		WHERE emp_no = '101010'
+) AND emp_no IN (
+		SELECT emp_no
+		FROM dept_emp
+		WHERE to_date > NOW()
 );
 
 #2. Find all the titles ever held by all current employees with the first name Aamod.
@@ -31,9 +34,12 @@ SELECT DISTINCT title
 FROM titles
 WHERE emp_no IN (
 		SELECT emp_no
+		FROM employees
+		WHERE first_name = 'Aamod'
+) AND emp_no IN (
+		SELECT emp_no
 		FROM dept_emp
-		JOIN employees USING(emp_no)
-		WHERE first_name = 'Aamod' AND to_date > NOW()
+		WHERE to_date > NOW()
 );
 
 #3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
@@ -48,30 +54,29 @@ WHERE emp_no NOT IN (
 
 #4. Find all the current department managers that are female. List their names in a comment in your code.
 SELECT first_name, last_name, gender
-FROM dept_manager dm
-JOIN employees e USING(emp_no)
-WHERE dm.to_date > NOW() AND emp_no IN (
-					SELECT emp_no
-					FROM employees
-					WHERE gender = 'F'
+FROM employees
+WHERE emp_no IN (
+		SELECT emp_no
+		FROM dept_manager
+		WHERE to_date > NOW()
+) AND emp_no IN (
+			SELECT emp_no
+			FROM employees
+			WHERE gender = 'F'
 );
 #Isamu Legleitner, Karsten Sigstam, Leon DasSarma, Hilary Kambil
-/* wrote subquery into solution, noticed it was more work, simplified query
-SELECT first_name, last_name, to_date, gender
-FROM dept_manager dm
-JOIN employees e USING(emp_no)
-WHERE dm.to_date > NOW() AND gender = 'F';
-*/
 
 #5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
-SELECT first_name, last_name, salary
+SELECT first_name, last_name
 FROM employees
-JOIN salaries s USING(emp_no)
-WHERE s.to_date > NOW() AND salary > (
-				SELECT AVG(salary)
-				FROM salaries
+WHERE emp_no IN (
+		SELECT emp_no
+		FROM salaries
+		WHERE to_date > NOW() AND salary > (
+		SELECT AVG(salary)
+		FROM salaries
 )
-ORDER BY salary;
+);
 
 #6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
 SELECT COUNT(salary)
